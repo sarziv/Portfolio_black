0cf9427c1c3776e5e105ddd5132286a0115ab4d0
sarziv <saruzivila12@gmail.com>
2019-04-26 18:58:32 +0300

    Small ui change
    home and about change

--- resources/views/info/about-info.blade.php
+++ resources/views/info/about-info.blade.php
@@ -2,44 +2,37 @@
 <div class="container text-center about-info">
     <div class="row">
         <div class="col-sm-6 about-info-text ">
-            <div class="lax" data-lax-preset="slalom fadeOut">
-                Me..<bR>
-                mE...<bR>
-                ME..!<bR>
+            <div class="lax" data-lax-preset="fadeOut">
+                Little<bR>
+                about<bR>
+                me...<bR>
             </div>
         </div>
         <div class="col-sm-6 about-info-bar lax" data-lax-preset="fadeOut">
-            Simple man who like web development.<br>
+            In love with web development.<br>
             Working with Laravel as Full stack developer.<br>
             Student at KTU for bachelor degree in IT.
         </div>
     </div>
 </div>
+
 <div class="container-fluid about-info-details">
     <div class="container about-info-details-font">
         I work with...
-                <div class="row about-info-img">
-                    <div class="col-sm-2">
-                        <img src="{{asset('icons/laravel.png')}}">
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="{{asset('icons/php.png')}}">
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="{{asset('icons/jquery.png')}}">
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="{{asset('icons/html5.png')}}">
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="{{asset('icons/css3.png')}}">
-                    </div>
-                    <div class="col-sm-2">
-                        <img src="{{asset('icons/js.png')}}">
-                    </div>
+        <div class=" about-info-img">
+            <div class="d-inline align-content-center">
+                <img src="{{asset('icons/laravel.png')}}">
+                <img src="{{asset('icons/php.png')}}">
+                <img src="{{asset('icons/jquery.png')}}">
+                <img src="{{asset('icons/html5.png')}}">
+                <img src="{{asset('icons/css3.png')}}">
+                <img src="{{asset('icons/js.png')}}">
             </div>
+
         </div>
+    </div>
 </div>
+
 <div class="container text-center about-info">
     <div class="row">
         <div class="col-sm-6 about-info-text extra-padd">
@@ -49,8 +42,9 @@
         </div>
         <div class="col-sm-6 about-info-bar lax" data-lax-preset="fadeOut">
             Mostly working as freelancer, with above mentioned technologies.<br>
-            Working with little projects. erm single man squad :)<br>
-            Fluent in english, as well as in 'GIT commit'.
+            Laravel [LAMP stack], CSS, HTML, Bootstrap, Git.<br>
+
+
         </div>
     </div>
 </div>
