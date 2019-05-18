e390b17c1e2d11a94e9cb3a5d9862ab8646add84
sarziv <saruzivila12@gmail.com>
2019-05-18 16:33:24 +0300

    project info change

--- resources/views/info/project-info-additional.blade.php
+++ resources/views/info/project-info-additional.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid project-info-details">
     <div class="container project-info-details-text">
-        Love, dynamic easy to understand and maintain projects.
+        Passion for dynamic scalable and maintainable projects.
         <a class="details-button" href="https://github.com/sarziv/Portfolio_black">Source code</a>
     </div>
 </div>
